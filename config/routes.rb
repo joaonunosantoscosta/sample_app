e17395bd62d1325da4a91ec72f5687af4d62832f
Joao Costa <joaonunosantoscosta@gmail.com>
2015-06-17 11:17:40 +0000

    commit para ir almocar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -19,6 +21,8 @@ Rails.application.routes.draw do
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
+  
+  get 'signup'  => 'users#new'
 
 
 
